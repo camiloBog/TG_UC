e7111d2538f57268ec6ece703749a5590559600a
CamiloBustamante <cbustamantes@ucentral.edu.co>
2016-10-25 14:01:26 -0500

    Adaptando el login

--- AngularJs/WebContent/assets/js/index.js
+++ AngularJs/WebContent/assets/js/index.js
@@ -7,24 +7,24 @@ $("#login-button").click(function(event){
 
 
 
-var mainApp = angular.module("mainApp", [ 'ngRoute' ]);
-
-mainApp.config([ '$routeProvider', function($routeProvider) {
-
-	$routeProvider.when('/inventario', {
-		templateUrl : 'modules/compras/inventario.html',
-	}).
-
-	when('/ordenCompra', {
-		templateUrl : 'modules/compras/ordenCompra.html',
-	}).
-
-	when('/regitraEntrada', {
-		templateUrl : 'modules/compras/registroEntrada.html',
-	}).
-
-	otherwise({
-		redirectTo : '/inventario'
-	});
-
-} ]);
+//var mainApp = angular.module("mainApp", [ 'ngRoute' ]);
+//
+//mainApp.config([ '$routeProvider', function($routeProvider) {
+//
+//	$routeProvider.when('/inventario', {
+//		templateUrl : 'modules/compras/inventario.html',
+//	}).
+//
+//	when('/ordenCompra', {
+//		templateUrl : 'modules/compras/ordenCompra.html',
+//	}).
+//
+//	when('/regitraEntrada', {
+//		templateUrl : 'modules/compras/registroEntrada.html',
+//	}).
+//
+//	otherwise({
+//		redirectTo : '/inventario'
+//	});
+//
+//} ]);
