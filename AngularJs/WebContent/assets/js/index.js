c0122cd1bc51b1595271327d9354e71113a8bd70
CamiloBustamante <cbustamantes@ucentral.edu.co>
2016-10-25 12:44:48 -0500

    Se ordenan algunos archivos del proyecto

---
+++ AngularJs/WebContent/assets/js/index.js
@@ -0,0 +1,30 @@
+$("#login-button").click(function(event){
+		 event.preventDefault();
+	 
+	 $('form').fadeOut(500);
+	 $('.wrapper').addClass('form-success');
+});
+
+
+
+var mainApp = angular.module("mainApp", [ 'ngRoute' ]);
+
+mainApp.config([ '$routeProvider', function($routeProvider) {
+
+	$routeProvider.when('/inventario', {
+		templateUrl : 'modules/compras/inventario.html',
+	}).
+
+	when('/ordenCompra', {
+		templateUrl : 'modules/compras/ordenCompra.html',
+	}).
+
+	when('/regitraEntrada', {
+		templateUrl : 'modules/compras/registroEntrada.html',
+	}).
+
+	otherwise({
+		redirectTo : '/inventario'
+	});
+
+} ]);
